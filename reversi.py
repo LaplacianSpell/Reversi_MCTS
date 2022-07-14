ffab3983cd393522c150cb6141ae92a2ad13d69a
LaplacianSpell <pd20@mails.tsinghua.edu.cn>
2022-07-14 15:38:18 +0800

    feat: Add the outline structure of `class MCST` in reversi.py
    Added outline structure:
      1. the attributes of class
      2. the method which you can input the position to set and return
      a new board
      3. the method which you can input board and player and return
      list of allowed position.
      4. the method which you can input board and return the winner.
      5. the method to tell where to move next (i.e.: the AI alg.)
      6. the simulation method (which is the core of AI alg.)

--- reversi.py
+++ reversi.py
@@ -32,32 +32,131 @@ def index_to_coord(i: int) -> Tuple[int, int]:
     return i >> 3, i & 7
 
 # class for restoration of board and do simulation.
-class MCTS:
-
-    def __init__(self, board):
-        self.boardArray = np.array(board) # 存储棋盘数组
-        self.states = [] # 存储状态树
-
-        self.seconds = 180 # 存储玩家走秒
-        self.calculation_time = datetime.timedelta(seconds=self.seconds) # 计算走秒时间
-
-        self.max_moves = 100 # 搜索树最大深度，可以修改
-
-        #节点信息
-        self.wins = {} # 字典，键是（玩家，玩家落子后的状态），值是获胜次数
-        self.plays = {} # 字典，键是（玩家，玩家落子后的状态），值是触摸当前节点次数
-
-        self.C = 1.414 # UCB1 评估函数的参数，可调节
-
+class MCTS(object):
     '''
     需要实现复盘功能，辅助模拟：
     1. 给定当前棋盘数组，返回当前落子玩家
     2. 给定当前棋盘数组与落子位置，返回新的棋盘数组
-    3. 给定当前棋盘数组，返回允许落子的位置元组的数组
+    3. 给定当前棋盘数组，返回允许落子的位置下标的数组
     4. 给定当前棋盘数组，返回对棋盘胜负的判断
     
     0. 需要实现模拟算法
     '''
+    def __init__(self, player, board, allow):
+
+        # 存储棋盘数组 numpy
+        self.mctsBoard = np.array(board) 
+
+        # 存储玩家 int 0 or 1
+        self.mctsPlayer = player 
+
+        # 存储允许落子的点 list
+        self.mcstAllow = np.array(list) 
+
+        # 存储状态树(记录每次落子的位置 int，也就是游戏过程)
+        self.states = [] 
+
+        # 存储玩家走秒
+        # 计算走秒时间
+        self.seconds = 180 
+        self.calculation_time = 18 
+
+        #节点信息
+        # 字典，键是（玩家编号，当前节点代表的落子位置），值是获胜次数
+        self.wins = {} 
+        # 字典，键是（玩家编号，当前节点代表的落子位置），值是触摸当前节点次数
+        self.plays = {} 
+
+        # UCB1 评估函数的参数，可调节
+        self.C = 1.4 
+
+    # def currentPlayer(self, board): # 似乎可以用一个标记变量实现
+    #     '''
+    #     arguments: board (numpy array)
+
+    #     returns: the player (0 or 1)
+    #     '''
+
+    def nextBoard(self, board, player, position):
+        '''
+        renew the board
+
+        arguments: 
+        - board (numpy array)
+        - player (int)
+        - position (int)
+
+        returns: board (numpy array)
+        '''
+        board[position] = player
+        return board    
+
+    def legalPlay(self, board, player):
+        '''
+        arguments: 
+        - board (numpy array)
+        - player (int)
+
+        returns: allow (numpy array)
+        '''
+        ######
+        allow = np.zeros(64)
+        return allow
+
+    def winner(self, board):
+        '''
+        arguments:
+        - board
+
+        return: winner (int)
+        '''
+        ######
+        return 1
+    
+    def getMove(self):
+        '''
+        the main realize of the game AI
+
+        return: move (int)
+        '''
+
+        # 落子记录的副本，避免直接修改
+        states = self.states[-1] 
+
+        # 当前落子的玩家
+        player = self.mctsPlayer 
+
+        # 允许的落子地点
+        legal = self.mcstAllow 
+
+        # 如果所有的位置都不能落子，直接返回
+        if not all(legal):
+            return
+
+        # 如果只有一个位置可以落子，那么只能下那个位置
+        if legal.count(True) == 1:
+            return legal.index(True)
+
+        # 如果有多个可能的位置，在允许的时间里一直进行模拟
+        begin = datetime.datetime.utcnow()
+        while datetime.datetime.utcnow() - begin < self.calculation_time:
+            self.run_simulation()
+
+        # 寻找当前的所有允许的落子位置
+        possibleMove = [(i, self.nextBoard(self.board, player, p)) for i, p in enumerate(legal) if p == True]
+
+        # 计算所有允许落子位置的胜率最大值，并记录
+        winPossibility, move = max(
+            (
+            self.wins.get((player, S), 0) /
+            self.plays.get((player, S), 1),
+            p
+            )
+            for p, S in possibleMove 
+        )
+
+        return move
+
 
 # modify reversi_ai function to implement your algorithm
 
@@ -70,10 +169,8 @@ def reversi_ai(player: int, board: List[int], allow: List[bool]) -> Tuple[int, i
 
     return: 落子坐标元组
     '''
-    
-
-    return (0.0)
-
+    MyBoard = MCTS(player, board, allow)
+    return MyBoard.getMove()
 
 # DO NOT MODIFY ANY CODE BELOW
 # **不要修改**以下的代码
